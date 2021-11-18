d89f93e5d0207572451694b401ae3f69e2479fc4
Bhavik Prajapati <rahulprajapati1665@gmail.com>
2021-11-18 21:46:10 +0530

    intial-commit

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro officia
+        provident tempore ad, earum iure natus similique accusamus maxime eos.
+        Perferendis harum reiciendis eos, excepturi natus ratione architecto
+        repellendus earum.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
